e918eb0f29a31b58fed9fff2d756783de9b31f40
denzow <denzow@gmail.com>
2021-09-16 18:23:30 +0900

    [add] input

--- redasql/command.py
+++ redasql/command.py
@@ -16,6 +16,8 @@ class MainCommand:
             redash_url=self.endpoint,
             api_key=self.api_key
         )
+        # TODO データソースID固定
+        self.datasource_id = 1
 
     def get_version(self):
         return self.client.get_version()
@@ -23,11 +25,23 @@ class MainCommand:
     def get_queries(self):
         return self.client.get_queries()
 
-    def execute_query(self):
-        return self.client.execute_query(query='SELECT * FROM country;', data_source_id=1)
+    def execute_query(self, query: str, data_source_id: int):
+        return self.client.execute_query(
+            query=query,
+            data_source_id=data_source_id,
+        )
+
+    def loop(self):
+        answer = input('SQL> ')
+        result = self.execute_query(
+            query=answer,
+            data_source_id=self.datasource_id
+        )
+        import json
+        print(json.dumps(result, indent=4))
 
 
-if __name__ == '__main__':
+def main():
     print(dedent("""
     ____          _                 _ 
     |  _ \ ___  __| | __ _ ___  __ _| |
@@ -42,3 +56,12 @@ if __name__ == '__main__':
     )
     print(f'server version: {command.get_version()}')
     # print(command.execute_query())
+    while True:
+        command.loop()
+
+
+
+
+
+if __name__ == '__main__':
+    main()
